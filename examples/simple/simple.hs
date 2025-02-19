ae09f4dcea344c35eaace05533f879fc9ee1847f
Tom McLaughlin <tom@codedown.io>
2025-02-18 19:54:18 -0800

    simple.hs: fix up and modernize

--- examples/simple/simple.hs
+++ examples/simple/simple.hs
@@ -1,17 +1,28 @@
 module Main where
 
-import System.Directory
-import System.IO
+import Control.Exception (bracket)
+import Control.Monad (void)
+import qualified Data.ByteString as BS
+import GHC.Foreign (withCString)
+import GHC.IO.Encoding (getFileSystemEncoding)
+import System.Directory (getHomeDirectory)
+import System.INotify (EventVariety(..), initINotify, addWatch, removeWatch)
 
-import System.INotify
 
 main :: IO ()
 main = do
     inotify <- initINotify
     print inotify
-    home <- getHomeDirectory
-    wd <- addWatch inotify [Open,Close,Access,Modify,Move] home print
-    print wd
-    putStrLn "Listens to your home directory. Hit enter to terminate."
-    getLine
-    removeWatch wd
+
+    home <- getHomeDirectory >>= toRawFilePath
+    bracket (addWatch inotify [Open, Close, Access, Modify, Move] home print) removeWatch $ \wd -> do
+      print wd
+
+      putStrLn "Listening to your home directory. Hit enter to terminate."
+      void getLine
+
+
+toRawFilePath :: FilePath -> IO BS.ByteString
+toRawFilePath fp = do
+  enc <- getFileSystemEncoding
+  withCString enc fp BS.packCString
